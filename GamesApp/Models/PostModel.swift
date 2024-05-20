f83f398bc17c08070f63a5e948e8bf6e62e8f200
yunusdvp <yunusemre7033@gmail.com>
2024-05-20 10:30:49 +0300

    Controller,ViewModel,Custom Cell created

---
+++ GamesApp/Models/PostModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostModel.swift
+//  GamesApp
+//
+//  Created by Yunus Emre ÖZŞAHİN on 19.05.2024.
+//
+
+import Foundation
---
+++ GamesApp/Views/CustomCells/GamesCollectionViewCell/GamesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GamesCollectionViewCell.swift
+//  GamesApp
+//
+//  Created by Yunus Emre ÖZŞAHİN on 19.05.2024.
+//
+
+import UIKit
+
+class GamesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
